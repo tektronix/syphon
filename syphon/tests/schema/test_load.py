2210ee034289557552d463a241c78b2d56ca2581
Evan Hall <evan.tom.hall@gmail.com>
2018-02-08 18:51:39 -0500

    New schema tests

---
+++ syphon/tests/schema/test_load.py
@@ -0,0 +1,27 @@
+"""syphon.tests.schema.test_load.py
+
+   Copyright (c) 2017-2018 Keithley Instruments, LLC.
+   Licensed under MIT (https://github.com/ehall/syphon/blob/master/LICENSE)
+
+"""
+import pytest
+from json import dumps, loads
+from sortedcontainers import SortedDict
+
+from syphon.schema import load
+
+class TestLoad(object):
+    multi_schema = SortedDict({'0': 'column2', '1': 'column4'})
+    single_schema = SortedDict({'0': 'column1'})
+
+    @pytest.mark.parametrize('schema', [
+        (single_schema),
+        (multi_schema)
+    ])
+    def test_load(self, schema, tmpdir):
+        tmpfile = tmpdir.mkdir('test_load').join('.schema.json')
+        tmpfile.write(dumps(schema))
+
+        actual = load(str(tmpfile))
+
+        assert SortedDict(loads(tmpfile.read())) == actual
---
+++ syphon/tests/schema/test_save.py
@@ -0,0 +1,45 @@
+"""syphon.tests.schema.test_save.py
+
+   Copyright (c) 2017-2018 Keithley Instruments, LLC.
+   Licensed under MIT (https://github.com/ehall/syphon/blob/master/LICENSE)
+
+"""
+import pytest
+from json import dumps, loads
+from sortedcontainers import SortedDict
+
+from syphon.schema import save, SchemaExistsError
+
+class TestSave(object):
+    multi_schema = SortedDict({'0': 'column2', '1': 'column4'})
+    single_schema = SortedDict({'0': 'column1'})
+
+    @pytest.mark.parametrize('schema, overwrite', [
+        (single_schema, False),
+        (multi_schema, True)
+    ])
+    def test_save_new(self, schema, overwrite, tmpdir):
+        tmpfile = tmpdir.mkdir('test_save').join('.schema.json')
+
+        save(schema, str(tmpfile), overwrite)
+
+        assert SortedDict(loads(tmpfile.read())) == schema
+
+    @pytest.mark.parametrize('schema', [
+        (single_schema),
+        (multi_schema)
+    ])
+    def test_save_overwrite(self, schema, tmpdir):
+        tmpfile = tmpdir.mkdir('test_save').join('.schema.json')
+        tmpfile.write('content')
+
+        save(schema, str(tmpfile), True)
+
+        assert SortedDict(loads(tmpfile.read())) == schema
+
+    def test_save_exists_fail(self, tmpdir):
+        tmpfile = tmpdir.mkdir('test_save').join('.schema.json')
+        tmpfile.write('content')
+
+        with pytest.raises(SchemaExistsError):
+            save(TestSave.single_schema, str(tmpfile), False)
