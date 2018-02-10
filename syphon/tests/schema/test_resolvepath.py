7149a7250a3bee4894d7c703f38b522a01bd3daa
Evan Hall <evan.tom.hall@gmail.com>
2018-02-10 12:07:27 -0500

    unit tests for schema.resolvepath

--- syphon/tests/schema/test_resolvepath.py
+++ syphon/tests/schema/test_resolvepath.py
@@ -4,51 +4,132 @@
    Licensed under MIT (https://github.com/ehall/syphon/blob/master/LICENSE)
 
 """
+from os.path import join
+
 import pytest
+from numpy import nan
 from pandas import DataFrame, read_csv
 from pandas.compat import StringIO
+from pandas.util.testing import makeCustomIndex
 from sortedcontainers import SortedDict
-
 from syphon.schema import resolve_path
 
 from .. import make_dataframe, make_dataframe_value
 
 class TestResolvePath(object):
-    @staticmethod
-    def invalid_value_map(row: int, col: int):
-        from numpy import nan
+    archive = '.\\'
+    headers = makeCustomIndex(7, 1, prefix='C')
 
+    multi_schema = SortedDict({
+        '0': headers[1],
+        '1': headers[3]
+    })
+    multi_schema2 = SortedDict({
+        '0': headers[0],
+        '1': headers[2],
+        '2': headers[3]
+    })
+    multi_schema3 = SortedDict({
+        '0': headers[1],
+        '1': headers[6]
+    })
+
+    single_schema = SortedDict({
+        '0': headers[0],
+    })
+    single_schema2 = SortedDict({
+        '0': headers[4]
+    })
+
+    @staticmethod
+    def data_gen_invalid(row: int, col: int):
+        valmap_invalid = [
+            ['val', 'val', 'xxx', 'val'],
+            ['val',   nan, 'val', 'val'],
+            ['val',   nan, 'val',   nan],
+            [  nan,   nan, 'val', 'xxx'],
+            ['xxx',   nan, 'val',   nan]
+        ]
+
+        if row < len(valmap_invalid):
+            if col < len(valmap_invalid[row]):
+                return valmap_invalid[row][col]
+        return make_dataframe_value(row, col)
+
+    @staticmethod
+    def data_gen(row: int, col: int):
         valmap = [
-            [None, None,  None, None],
-            [None,  nan,  None, None],
-            [None,  nan,  None,  nan],
-            [ nan,  nan, 'val',  nan],
-            [ nan,  nan, 'val',  nan]
+            ['val', 'val', 'val', 'val'],
+            ['val',   nan, 'val', 'val'],
+            ['val',   nan, 'val',   nan],
+            [  nan,   nan, 'val',   nan],
+            [  nan,   nan, 'val',   nan]
         ]
 
         if row < len(valmap):
             if col < len(valmap[row]):
-                if valmap[row][col] is not None:
-                    if valmap[row][col] is nan:
-                        return nan
-                    else:
-                        return valmap[row][col]
+                return valmap[row][col]
         return make_dataframe_value(row, col)
 
     @staticmethod
-    def valid_value_map(row: int, col: int):
-        from numpy import nan
-
+    def data_gen_normalizable(row: int, col: int):
         valmap = [
-            [None, None, None, None],
-            [None,  nan, None, None],
-            [None,  nan, None,  nan],
-            [ nan,  nan, None,  nan],
-            [ nan,  nan, None,  nan]
+            ['Value 1', 'Value 1', 'Value 1', 'Value 1'],
+            ['Value 1',       nan, 'Value 1', 'Value 1'],
+            ['Value 1',       nan, 'Value 1',       nan],
+            [      nan,       nan, 'Value 1',       nan],
+            [      nan,       nan, 'Value 1',       nan]
         ]
 
         if row < len(valmap):
             if col < len(valmap[row]):
-                if valmap[row][col] is not None:
-                    return nan
+                return valmap[row][col]
         return make_dataframe_value(row, col)
+
+    @pytest.mark.parametrize('schema, expected', [
+        (single_schema, join(archive, 'val')),
+        (multi_schema, join(archive, 'val', 'val')),
+        (multi_schema2, join(archive, 'val', 'val', 'val')),
+    ])
+    def test_resolve_path(self, schema, expected):
+        data = make_dataframe(5, 4, data_gen_f=TestResolvePath.data_gen)
+
+        actual = resolve_path(self.archive, schema, data)
+
+        assert actual == expected
+
+    @pytest.mark.parametrize('schema, expected', [
+        (single_schema, join(archive, 'value_1')),
+        (multi_schema, join(archive, 'value_1', 'value_1')),
+        (multi_schema2, join(archive, 'value_1', 'value_1', 'value_1'))
+    ])
+    def test_resolve_path_normalized(self, schema, expected):
+        data = make_dataframe(5, 4,
+                              data_gen_f=TestResolvePath.data_gen_normalizable)
+
+        actual = resolve_path(self.archive, schema, data)
+
+        assert actual == expected
+
+    @pytest.mark.parametrize('schema', [
+        single_schema2,
+        multi_schema3,
+    ])
+    def test_resolve_path_indexerror(self, schema):
+        data = make_dataframe(5, 4, data_gen_f=TestResolvePath.data_gen)
+
+        with pytest.raises(IndexError):
+            resolve_path(self.archive, schema, data)
+
+    @pytest.mark.parametrize('schema', [
+        single_schema,
+        multi_schema,
+        multi_schema2,
+    ])
+    def test_resolve_path_valueerror(self, schema):
+        data = make_dataframe(5, 4,
+                              data_gen_f=TestResolvePath.data_gen_invalid)
+
+        with pytest.raises(ValueError):
+            resolve_path(self.archive, schema, data)
+
