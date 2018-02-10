f9ea4701ce9067c773162ac8af14561630cf5e65
Evan Hall <evan.tom.hall@gmail.com>
2018-02-10 12:25:01 -0500

    syphon.Context unit tests

---
+++ syphon/tests/test_context.py
@@ -0,0 +1,36 @@
+"""syphon.tests.test_context.py
+
+   Copyright (c) 2017-2018 Keithley Instruments, LLC.
+   Licensed under MIT (https://github.com/ehall/syphon/blob/master/LICENSE)
+
+"""
+from sortedcontainers import SortedDict
+
+from syphon import Context
+
+def test_context_archive_property_default():
+    assert Context().archive is None
+
+def test_context_cache_property_default():
+    assert Context().cache is None
+
+def test_context_data_property_default():
+    assert Context().data is None
+
+def test_context_meta_property_default():
+    assert Context().meta is None
+
+def test_context_overwrite_property_default():
+    assert Context().overwrite is False
+    assert isinstance(Context().overwrite, bool)
+
+def test_context_schema_property_default():
+    assert Context().schema is None
+
+def test_context_schema_file_property_default():
+    assert Context().schema_file == '.schema.json'
+    assert isinstance(Context().schema_file, str)
+
+def test_context_verbose_property_default():
+    assert Context().verbose is False
+    assert isinstance(Context().verbose, bool)
