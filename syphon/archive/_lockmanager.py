cd14a054debdff6a19b61cd77bf100472a00bca8
Evan Hall <evan.tom.hall@gmail.com>
2018-02-17 10:35:43 -0500

    LockManager.release checks for directory argument

--- syphon/archive/_lockmanager.py
+++ syphon/archive/_lockmanager.py
@@ -4,7 +4,7 @@
    Licensed under MIT (https://github.com/ehall/syphon/blob/master/LICENSE)
 
 """
-from os.path import abspath
+from os.path import abspath, isdir, join
 
 class LockManager(object):
     """Lock file helper.
@@ -57,8 +57,6 @@ class LockManager(object):
         Returns:
             str: Absolute filepath of the created lock file.
         """
-        from os.path import join
-
         filepath = join(abspath(path), self.filename)
 
         try:
@@ -72,12 +70,16 @@ class LockManager(object):
         return filepath
 
     def release(self, filepath: str):
-        """Remove the given lock file.
+        """Remove the given lock file or the lock file in the given
+        directory.
 
         Args:
             filepath (str): Location of a lock file.
         """
         fullpath = abspath(filepath)
+
+        if isdir(fullpath):
+            fullpath = join(fullpath, self._filename)
 
         if fullpath in self._locks:
             self._locks.remove(fullpath)
