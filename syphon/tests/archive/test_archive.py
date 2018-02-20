19630ba9fb7b726343b5fc332c4ded408d210819
Evan Hall <evan.tom.hall@gmail.com>
2018-02-20 17:17:51 -0500

    Added no schema archive test for iris dataset

--- syphon/tests/archive/test_archive.py
+++ syphon/tests/archive/test_archive.py
@@ -60,6 +60,45 @@ class TestArchive(object):
         assert expected_paths == actual_paths
         assert_frame_equal(expected_frame, actual_frame)
 
+    def test_archive_iris_no_schema(self, archive_dir, overwrite_fixture):
+        context = Context()
+        context.archive = archive_dir
+        context.data = os.path.join(get_data_path(), 'iris.csv')
+        context.overwrite = overwrite_fixture
+        context.schema = SortedDict()
+
+        expected_frame = DataFrame(read_csv(context.data, dtype=str))
+
+        expected_paths = SortedList([
+            os.path.join(str(context.archive), 'iris.csv')
+        ])
+
+        if context.overwrite:
+            for e in expected_paths:
+                path = archive_dir.new()
+                path.mkdir(os.path.basename(os.path.dirname(e)))
+                with open(e, mode='w') as f:
+                    f.write('content')
+
+        archive(context)
+
+        actual_frame = DataFrame()
+        actual_paths = SortedList()
+        for root, _, files in os.walk(context.archive):
+            for f in files:
+                if '.csv' in f:
+                    filepath = os.path.join(root, f)
+                    actual_paths.add(filepath)
+                    actual_frame = concat([
+                        actual_frame,
+                        DataFrame(read_csv(filepath, dtype=str))
+                    ])
+
+        actual_frame.reset_index(drop=True, inplace=True)
+
+        assert expected_paths == actual_paths
+        assert_frame_equal(expected_frame, actual_frame)
+
     def test_archive_fileexistserror(self, archive_dir):
         context = Context()
         context.archive = archive_dir
